523b3f4a4d7a63635be48a4a552d2f3aa8a7c4b7
Seedgou <i@zczc.cz>
2022-10-30 14:51:41 +0000

    getMapMin -> getMapMinOrZero

--- src/parallel-get/range/selectRangeStrategy.ts
+++ src/parallel-get/range/selectRangeStrategy.ts
@@ -1,12 +1,12 @@
 import type { RangeIndex } from "./types";
-import { getMapMin } from "./utils";
+import { getMapMinOrZero } from "./utils";
 
 const threshold = 10;
 
 export function defaultSelectRangeStrategy(
   downloaderCounter: Map<RangeIndex, number>
 ): RangeIndex {
-  const minRangeIndex = getMapMin(downloaderCounter);
+  const minRangeIndex = getMapMinOrZero(downloaderCounter);
   const firstIndex = downloaderCounter.keys().next().value as number;
 
   if (minRangeIndex - firstIndex > threshold) {
--- src/parallel-get/range/utils.ts
+++ src/parallel-get/range/utils.ts
@@ -1,7 +1,11 @@
-export function getMapMin<T>(map: Map<T, number>): T {
+export function getMapMinOrZero<T>(map: Map<T, number>): T {
   let minKey: T;
   let minVal: number | undefined;
   for (const [key, val] of map) {
+    if (val === 0) {
+      return key;
+    }
+
     if (minVal === undefined || val < minVal) {
       minKey = key;
       minVal = val;

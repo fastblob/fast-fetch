2bf003e49d45a87e0661082372f738e9a226c451
Seedgou <i@zczc.cz>
2022-10-30 07:48:55 +0000

    only GET requests are parallelized

--- src/parallel-fetch.ts
+++ src/parallel-fetch.ts
@@ -1,3 +1,5 @@
+import { parallelGet } from './parallel-get';
+
 type FetchParams = Parameters<typeof fetch>;
 type FetchReturn = ReturnType<typeof fetch>;
 
@@ -5,5 +7,11 @@ export function parallelFetch(
   input: FetchParams[0],
   init?: FetchParams[1]
 ): FetchReturn {
-  return fetch(input, init);
+  const method = (init && init.method) || "GET";
+  if (method === "GET") {
+    // only GET requests are parallelized
+    return parallelGet(input, init);
+  } else {
+    return fetch(input, init);
+  }
 }

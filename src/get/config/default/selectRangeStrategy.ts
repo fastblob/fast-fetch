b9a145a1ee08cd146cda1817589520297cbc8678
Seedgou <i@zczc.cz>
2022-11-07 07:40:29 +0000

    update selectRangeStrategy for better performance

--- src/get/config/default/selectRangeStrategy.ts
+++ src/get/config/default/selectRangeStrategy.ts
@@ -1,16 +1,16 @@
 type RangeIndex = number
 
-const threshold = 10
+// const threshold = 10
 
 export function defaultSelectRangeStrategy (
   downloaderCounter: Map<RangeIndex, number>
 ): RangeIndex {
   const minRangeIndex = getMapMinOrZero(downloaderCounter)
-  const firstIndex = downloaderCounter.keys().next().value as number
+  // const firstIndex = downloaderCounter.keys().next().value as number
 
-  if (minRangeIndex - firstIndex > threshold) {
-    return firstIndex
-  }
+  // if (minRangeIndex - firstIndex > threshold) {
+  //   return firstIndex
+  // }
 
   return minRangeIndex
 }
